dd3d2778910021d123793a3b191748682f8f4995
Gnosis #anoncoin Freenode IRC <gnosis@anoncoin.net>
2014-05-18 19:59:22 +0000

    WIP

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+'use strict';
+
 var sodium = require('sodium');
 var bigint = require('bigint');
 var app = require('express')();
 
+app.disable('x-powered-by');    // don't reveal that we're using Express
+console.log('GNOSIS YEAH: ' + app.disabled('x-powered-by'));
+
 app.get('/', function(req,res){
+    console.log('GNOSIS YEAH: ' + app.disabled('x-powered-by'));
     res.send('hello world');
 });
 

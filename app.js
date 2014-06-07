7974f4d0d16e53b9e825d03040c9fc5d90c2c8de
Gnosis #anoncoin Freenode IRC <gnosis@anoncoin.net>
2014-06-07 13:46:10 -0700

    wrote more

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 'use strict';
 
 var fs = require('fs');
+var assert = require('assert');
+var _ = require('underscore');
 var sodium = require('sodium');
 var bigint = require('bigint');
 var restify = require('restify');
 
-var secret = new Buffer(fs.readFileSync('server.sec'), 'base64');
+var secret = new Buffer(fs.readFileSync('server.sec', 'utf8'), 'base64');
 
 var app = restify.createServer();
+var msg = require('./sodium_msg')(secret);
+var fromClient = msg.fromClient;
+var toClient = msg.toClient;
+
+
+var client_pubkey = new Buffer('PIMZgpdx9D374F1sI79SZIXQUaBJIsQ5BxBOnZvHISI=', 'base64');//XXX FIXME
 
 app.post('/getwork', function(req,res){
-  console.log('req.body is %j', req.body);
+  if (!_.isObject(req.body)) return res.send(400);
+  var nick = req.body.nick;
+  var decrypted = fromClient(req.body.m, client_pubkey);
+
+  console.log('FROM nick %s: /getwork request DECRYPTED %j', nick, decrypted);
   res.json({msg:'hello world'});
 });
 

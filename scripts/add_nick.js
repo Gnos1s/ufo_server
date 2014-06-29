a2119ac53b7666786805a166291dc08d806c2cda
Gnosis #anoncoin Freenode IRC <gnosis@anoncoin.net>
2014-06-29 15:24:29 -0700

    add_nick now takes a file path

--- scripts/add_nick.js
+++ scripts/add_nick.js
@@ -1,20 +1,25 @@
 'use strict';
 
+var yaml = require('js-yaml');
+
 var DB_PATH = 'state.leveldb';
 var Db = require('../db');
 
-if (process.argv.length !== 4) {
-  console.log('Usage: node scripts/add_nick.js <nick> <pubkey>');
+if (process.argv.length !== 3) {
+  console.log('Usage: node scripts/add_nick.js <config.yml>');
   process.exit(1);
 }
+
+var config_file = process.argv[2];
+var config = yaml.safeLoad(fs.readFileSync(config_file, 'utf8'));
+var nick = config.nick;
+var pubkey = config.pubkey;
 
 var db = Db(DB_PATH, function(e) {
   console.error('ERROR opening database "%s": %s', DB_PATH, e.stack || e);
   process.exit(1);
 });
 
-var nick = process.argv[2];
-var pubkey = process.argv[3];
 db.setPublicKey(nick, pubkey, function(err) {
   if (err) {
     console.error('ERROR: %s', err.message || err);

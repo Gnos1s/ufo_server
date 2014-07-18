6ee8189d4a64d44e377e75d85b1f9e170f3cff69
Gnosis #anoncoin Freenode IRC <gnosis@anoncoin.net>
2014-07-18 01:06:28 +0000

    fix one-time B1 doubling hotfix

--- scripts/admin_tool/hotfixes/double_B1.js
+++ scripts/admin_tool/hotfixes/double_B1.js
@@ -1,12 +1,12 @@
 "use strict"
 
-console.log("Inside eval! :-)")
+console.log("%s: DOUBLED ALL B1");
 
 var original_b1_ufos = _.clone(b1_ufos);
 var new_b1_ufos = [];
 
 original_b1_ufos.forEach(function(B1, i){
-  B1 *= 2;
+  if (r_ufos[i]) B1 *= 2;
   new_b1_ufos.push(B1);
   b1_ufos[i] = B1;
 });
